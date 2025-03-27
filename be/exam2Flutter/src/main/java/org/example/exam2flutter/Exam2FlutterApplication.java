906cc24fb8a1cf3127b46592dc7c78492f354a89
HÙNG ĐINH TUẤN <hungtuandinhbg@gmail.com>
2025-03-27 19:07:28 +0700

    fcm

---
+++ be/exam2Flutter/src/main/java/org/example/exam2flutter/Exam2FlutterApplication.java
@@ -0,0 +1,13 @@
+package org.example.exam2flutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam2FlutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam2FlutterApplication.class, args);
+    }
+
+}
---
+++ be/exam2Flutter/src/test/java/org/example/exam2flutter/Exam2FlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exam2flutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam2FlutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
